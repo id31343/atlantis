b527897f30fcd030692191e7649ee7a91562ee45
Vadim <vadim.baranovsky@innowise-group.com>
2023-12-20 08:16:55 +0100

    Remove user data

--- terraform.tf
+++ terraform.tf
@@ -30,8 +30,8 @@ provider "aws" {
     tags = var.default_tags
   }
 
-  skip_metadata_api_check     = true
-  skip_region_validation      = true
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
+  # skip_metadata_api_check     = true
+  # skip_region_validation      = true
+  # skip_credentials_validation = true
+  # skip_requesting_account_id  = true
 }
